9c89eb7f3098f9c07657308cf5a8d28765a7bf9a
Jonte Good <JG056@atcltd.atc.org.nz>
2023-05-12 19:14:04 +1200

    Cleaned up code

--- Program.cs
+++ Program.cs
@@ -31,12 +31,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.UseSwagger();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
